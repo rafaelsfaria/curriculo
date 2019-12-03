81a232601991d19cd6dbd9dd91908d540403bcad
Rafael Faria <rafaelsanfaria@gmail.com>
2019-12-03 17:56:28 -0300

    updated path

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
-
+const path = require('path')
 const app = express();
 
+app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs');
-app.use(express.static('public'));
+app.use(express.static( path.join(__dirname,'public')))
 
 app.get('/', (req, res) => {
   res.render('index');
